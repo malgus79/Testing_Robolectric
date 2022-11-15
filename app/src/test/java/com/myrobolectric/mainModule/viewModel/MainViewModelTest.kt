008cd6af75db5c4f54f824252feb825be33ac8a2
Gustavo Maldonado <gamcba7982@gmail.com>
2022-11-15 16:08:32 -0300

    AndroidViewModel added

--- app/src/test/java/com/myrobolectric/mainModule/viewModel/MainViewModelTest.kt
+++ app/src/test/java/com/myrobolectric/mainModule/viewModel/MainViewModelTest.kt
@@ -1,3 +1,41 @@
 package com.myrobolectric.mainModule.viewModel
 
-class MainViewModelTest
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.hamcrest.MatcherAssert
+import org.hamcrest.Matchers
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.annotation.Config
+
+@Config(sdk = [29])
+@RunWith(AndroidJUnit4::class)
+class MainViewModelTest{
+    //arquitectura de componentes agregada para el testing
+    @get:Rule
+    var instantExcecutorRule = InstantTaskExecutorRule()
+
+    //este test es para ViewModel con constructor, recibe Application y ademas extiende de
+    //AndroidViewModel y tiene un repositorio qeu tambien depende de la application
+
+    //test: evaluar el mensaje de bienvenida
+    @Test
+    fun checkWelcomeTest(){
+        val mainViewModel = MainViewModel(ApplicationProvider.getApplicationContext())
+        val observer = Observer<Boolean>{}  //<T> debe ser el mismo declarado en el liveData
+
+        try {
+            mainViewModel.isWelcome().observeForever(observer)
+            val result = mainViewModel.isWelcome().value
+
+            //sintaxis: assertThat(Valor Actual, Valor Esperado)
+            MatcherAssert.assertThat(result, Matchers.not(Matchers.nullValue()))  //que no sea null
+            MatcherAssert.assertThat(result, Matchers.`is`(true))  //asegurar que sea boolean (true)
+        } finally {
+            mainViewModel.isWelcome().removeObserver(observer)
+        }
+    }
+}
