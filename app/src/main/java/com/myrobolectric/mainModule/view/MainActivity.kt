c96563460cb018587665389f5397a2b36e2d4289
Gustavo Maldonado <gamcba7982@gmail.com>
2022-11-18 19:22:57 -0300

    Add menu

--- app/src/main/java/com/myrobolectric/mainModule/view/MainActivity.kt
+++ app/src/main/java/com/myrobolectric/mainModule/view/MainActivity.kt
@@ -2,6 +2,8 @@ package com.myrobolectric.mainModule.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
 import com.google.android.material.snackbar.Snackbar
@@ -78,5 +80,22 @@ class MainActivity : AppCompatActivity(), OnClickListener {
 
     override fun onLongClick(product: Product) {
         viewModel.deleteProduct(product)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val msg = when(item.itemId){
+            R.id.action_history -> getString(R.string.main_msg_go_history)
+            R.id.action_exit -> getString(R.string.main_msg_go_exit)
+            else -> getString(R.string.main_msg_not_found)
+        }
+
+        Snackbar.make(binding.root, msg, Snackbar.LENGTH_SHORT).show()
+
+        return super.onOptionsItemSelected(item)
     }
 }
