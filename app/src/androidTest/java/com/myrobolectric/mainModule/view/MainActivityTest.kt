740ff1900d7c704aa65b076cce2d08bfb2313e6d
Gustavo Maldonado <gamcba7982@gmail.com>
2022-11-18 19:33:16 -0300

    Test Icon on ActionBar

--- app/src/androidTest/java/com/myrobolectric/mainModule/view/MainActivityTest.kt
+++ app/src/androidTest/java/com/myrobolectric/mainModule/view/MainActivityTest.kt
@@ -1,24 +1,14 @@
 package com.myrobolectric.mainModule.view
 
 
-import android.view.View
-import android.view.ViewGroup
-import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.*
-import androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import com.myrobolectric.R
-import org.hamcrest.Description
-import org.hamcrest.Matcher
-import org.hamcrest.Matchers.`is`
-import org.hamcrest.Matchers.allOf
-import org.hamcrest.TypeSafeMatcher
-import org.hamcrest.core.IsInstanceOf
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -27,63 +17,22 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class MainActivityTest {
 
-    @Rule
-    @JvmField
-    var mActivityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
+    @get:Rule
+    var activityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
 
+    //test: evento de click en el snackbar retornando un mensaje
     @Test
-    fun mainActivityTest() {
-        val recyclerView = onView(
-            allOf(
-                withId(R.id.recyclerView),
-                childAtPosition(
-                    withClassName(`is`("androidx.constraintlayout.widget.ConstraintLayout")),
-                    0
-                )
-            )
-        )
-        recyclerView.perform(actionOnItemAtPosition<ViewHolder>(1, click()))
+    fun actionBar_menuItemClick_returnsMsg() {
+        onView(withId(R.id.recyclerView)).perform(click())
 
-        val materialCheckBox = onView(
-            allOf(
-                withId(R.id.cbFavorite),
-                childAtPosition(
-                    childAtPosition(
-                        withClassName(`is`("com.google.android.material.card.MaterialCardView")),
-                        0
-                    ),
-                    3
-                ),
-                isDisplayed()
-            )
-        )
-        materialCheckBox.perform(click())
+        onView(withId(R.id.action_history)).perform(click())
 
-        val checkBox = onView(
-            allOf(
-                withId(R.id.cbFavorite),
-                withParent(withParent(IsInstanceOf.instanceOf(androidx.cardview.widget.CardView::class.java))),
-                isDisplayed()
-            )
-        )
-        checkBox.check(matches(isDisplayed()))
-    }
+//        var snackbarMsg = ""
+//        activityScenarioRule.scenario.onActivity { activity ->
+//            snackbarMsg = activity.resources.getString(R.string.main_msg_go_history)
+//        }
 
-    private fun childAtPosition(
-        parentMatcher: Matcher<View>, position: Int
-    ): Matcher<View> {
-
-        return object : TypeSafeMatcher<View>() {
-            override fun describeTo(description: Description) {
-                description.appendText("Child at position $position in parent ")
-                parentMatcher.describeTo(description)
-            }
-
-            public override fun matchesSafely(view: View): Boolean {
-                val parent = view.parent
-                return parent is ViewGroup && parentMatcher.matches(parent)
-                        && view == parent.getChildAt(position)
-            }
-        }
+        onView(withId(com.google.android.material.R.id.snackbar_text))
+            .check(matches(withText("Go to history…")))
     }
 }
--- app/src/androidTest/java/com/myrobolectric/mainModule/view/adapters/ProductAdapterTest.kt
+++ app/src/androidTest/java/com/myrobolectric/mainModule/view/adapters/ProductAdapterTest.kt
@@ -1,15 +1,11 @@
 package com.myrobolectric.mainModule.view.adapters
 
-import android.view.View
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.PerformException
-import androidx.test.espresso.UiController
-import androidx.test.espresso.ViewAction
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.longClick
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions.*
-import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -17,7 +13,6 @@ import androidx.test.filters.LargeTest
 import com.myrobolectric.R
 import com.myrobolectric.mainModule.view.MainActivity
 import com.myrobolectric.mainModule.view.clickInChild
-import org.hamcrest.Matcher
 import org.hamcrest.Matchers.*
 import org.junit.Assert.fail
 import org.junit.Rule
